706761aec33c25b3ca96edf8ac1d19f7472c4e2f
Schir <schirsa@gmail.com>
2022-09-18 11:00:01 -0600

    reading more about go

--- extracurricular work/go/hello.go
+++ extracurricular work/go/hello.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
@@ -18,7 +19,20 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+type Vertex struct {
+	X int
+	Y int
+}
+
 func main() {
+	//defer waits until the rest of the function has returned
+	defer fmt.Println("...the aristocrats!")
+
+	//deferred statements are pushed onto a stack
+	//they execute in last-in-first-out order
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
 	//utf-8 support is built in by default
 	fmt.Println("おはよう、ベトナムw")
 
@@ -46,4 +60,50 @@ func main() {
 	//short variable initialization (only available in functions)
 	h, w = swap(h, w)
 	fmt.Println(h, w)
+
+	//for loops
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	//for is also a while loop. can also just write for{} for an infinite loop
+	sum = 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+
+	fmt.Print("Go runs on ")
+	//switch statements don't have break;
+	//can't use fallthrough behavior
+	//omitting the condition just evaluates to true
+	//you can use this to write if/then/else chains more cleanly
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Printf("%s.\n", os)
+	}
+	//pointer initialization
+	var pSum *int = &sum
+	//pointer value and dereferenced pointer value
+	fmt.Println(pSum, *pSum)
+
+	//initializing structs
+	v := Vertex{1, 2}
+	v.X = 8
+	fmt.Println(Vertex{1, 2})
+	fmt.Println(v.X)
+	//arrays
+	var hw [2]string
+	hw[0] = "hellllo"
+	hw[1] = "worrrld"
+	fmt.Println(hw[0], hw[1])
+	fmt.Println(hw)
+	primes := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
 }
